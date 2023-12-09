fca149008a992585770c531061f91efc8a917c5c
arunshekhar <arunshekhar001@gmail.com>
2023-12-09 11:07:11 +0530

    slash command added

---
+++ discord-bot/commands/submitEoa.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+const submitEoa = new SlashCommandBuilder()
+  .setName("eoa")
+  .setDescription("set your Ethereum address")
+  .addStringOption((option) =>
+    option
+      .setName("eoa")
+      .setDescription("set your Ethereum address")
+      .setRequired(true)
+  );
+exports.submitEoa = submitEoa.toJSON();
--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,7 +1,14 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Routes, Events } = require("discord.js");
+const { submitEoa } = require("./commands/submitEoa");
+const { REST } = require("@discordjs/rest");
 require("dotenv").config();
 
-const token = process.env.BOT_TOKEN;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const rest = new REST({ version: "10" }).setToken(BOT_TOKEN);
+
 const { GuildMembers, GuildMessages, Guilds, MessageContent } =
   GatewayIntentBits;
 const client = new Client({
@@ -12,4 +19,31 @@ client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.login(token);
+client.login(BOT_TOKEN);
+
+client.on("messageCreate", (message) => {
+  if (message.content === "Moye") {
+    message.channel.send("Moye :(");
+  }
+});  
+
+client.on("interactionCreate", (interaction) => {
+  if (interaction.commandName === "eoa") {
+    interaction.reply("EOA registered");
+  }
+});
+
+async function main() {
+  const commands = [submitEoa];
+  try {
+    console.log("Started refreshing application (/) commands.");
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+    client.login(BOT_TOKEN);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
